86098684240bc89bc25db12c7cd5d27593432e90
Jess Ogwu <ogwujessica2222@gmail.com>
2022-10-18 23:41:08 +0100

    created book_ikeja method

--- hotels/bookings.py
+++ hotels/bookings.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from hotels.bookings_lagos import Booking_Lagos
+from hotels.bookings_ikeja import Booking_Ikeja
 
 class Booking(webdriver.Chrome):
     def __init__(self):
@@ -14,14 +14,15 @@ class Booking(webdriver.Chrome):
         chrome_options.add_experimental_option("detach", True)
         driver = webdriver.Chrome(executable_path='C:\chromedriver_win32\chromedriver', options=chrome_options)
         self.driver=driver
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(50)
         self.driver.maximize_window()
 
     def find_hotel(self):
-        self.driver.get('https://hotels.ng/')
+        self.driver.get('https://hotels.ng/hotels-in-lagos')
         
 
-    def book_lagos(self):
-        lagos_hotel=Booking_Lagos(driver=self.driver)
-        lagos_hotel.hotels_lagos()
-        lagos_hotel.select_budget(budget=3000)
+    def book_ikeja(self, budget):
+        ikeja_hotel=Booking_Ikeja(driver=self.driver)
+        ikeja_hotel.select_location()
+        ikeja_hotel.select_budget(budget=budget)
+        ikeja_hotel.send_email()
